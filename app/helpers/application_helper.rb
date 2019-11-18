62420fe8b4aaf45bbde24769705e3aded6fbf7ea
Abdul Barek <barek2k2@gmail.com>
2019-11-17 22:18:46 -0700

    breadcrum implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def breadcrumbs
+    html = ""
+    if params[:controller] == 'manage/contents'
+      html << link_to("Courses -> ", manage_courses_path)
+      html << link_to("Chapters -> ", manage_course_chapters_path(@course))
+    elsif params[:controller] == 'manage/chapters'
+      html << link_to("Courses -> ", manage_courses_path)
+    end
+    html.html_safe
+  end
 end
